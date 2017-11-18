9943366d9b284b9b24a9b7ac0c7c8132146974c4
cyronquillo <cbronquillo@up.edu.ph>
2017-11-18 11:30:45 +0800

    removed synchronized(this);

--- chat-system/ClientThread.java
+++ chat-system/ClientThread.java
@@ -9,15 +9,11 @@ public class ClientThread extends Thread{
 	private BufferedReader input;
 	private PrintStream output;
 	private Socket cSocket;
-	private ArrayList<ClientThread> threads;
-
-	public ClientThread(Socket cSocket, ArrayList<ClientThread> threads){
+	public ClientThread(Socket cSocket){
 		this.cSocket = cSocket;
-		this.threads = threads;
 	}
 
 	public void run(){
-		ArrayList<ClientThread> threads = this.threads; // personal list of threads 
 		try{
 			input = new BufferedReader(new InputStreamReader(cSocket.getInputStream()));
 			output = new PrintStream(cSocket.getOutputStream());
@@ -28,39 +24,39 @@ public class ClientThread extends Thread{
 
 			//notify other clients that a new client has entered
 			output.println("Hello " + usr + "!\n[NOTE: type 'bye' and press Enter to leave the room anytime]");
-			synchronized (this) {
-				for(int i = 0; i < threads.size(); i++){
-					if(threads.get(i) != this){
-						threads.get(i).output.println(usr +" has entered the chat room!");
-					}
+			
+			for(int i = 0; i < Server.threads.size(); i++){
+				if(Server.threads.get(i) != this){
+					Server.threads.get(i).output.println(usr +" has entered the chat room!");
 				}
 			}
 
-
+			// message sending
 			while(true){
 				String line = input.readLine();
 				if(line.equals("bye")) {
 					break;
 				}
 				synchronized(this){
-					for(int i = 0; i < threads.size(); i++){
-						threads.get(i).output.println("[" + usr + "]: " + line);
+					for(int i = 0; i < Server.threads.size(); i++){
+						Server.threads.get(i).output.println("[" + usr + "]: " + line);
 					}
 				}
 			}
-			synchronized(this){
-				for(int i = 0; i < threads.size(); i++){
-					if(threads.get(i) != this){
-						threads.get(i).output.println(usr + " has left the chat room :(");
-					}
+
+
+			// notify other clients that a client has left
+			for(int i = 0; i < Server.threads.size(); i++){
+				if(Server.threads.get(i) != this){
+					Server.threads.get(i).output.println(usr + " has left the chat room :(");
 				}
 			}
+
 			output.println("You have left the chat room.");
 
 
-			synchronized(this){
-				while(threads.size() > 0) threads.remove(0);
-			}
+			Server.threads.remove(this);
+
 
 			input.close();
 			output.close();
